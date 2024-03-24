7e1031ca43230d85d9a5232df3633d7b622b260a
Jinad Tope Isaac <jinadtope66@gmailcom>
2024-03-24 16:34:27 +0100

    survey

--- src/pages/Apply.jsx
+++ src/pages/Apply.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import NikeOrderForm from './Embedded'
 
 
 const Apply = () => {
@@ -7,13 +8,18 @@ const Apply = () => {
         <div className=''>j</div>
         <p className='mt-20 text-center underline font-bold'>Apply here </p>
 
-        <iframe
+    <div className="container">
+        {/* <iframe
       title="Nike Product Order Form"
       className='mb-5'
       src="https://form.jotform.com/240826909263260"
       style={{ minWidth: '100%', maxWidth: '100%', height: '539px', border: 'none' }}
       scrolling="no"
-    />
+    /> */}
+
+    <NikeOrderForm/>
+
+    </div>
     </>
   )
 }
