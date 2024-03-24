fa70d4059cb8aa8f14cb88c8bc5c784e847b33c7
Jinad Tope Isaac <jinadtope66@gmailcom>
2024-03-24 16:37:10 +0100

    survey

--- src/pages/Apply.jsx
+++ src/pages/Apply.jsx
@@ -1,25 +1,12 @@
 import React from 'react'
-import NikeOrderForm from './Embedded'
+import NikeOrderForm from '../components/Embedded'
+
 
 
 const Apply = () => {
   return (
     < >
-        <div className=''>j</div>
-        <p className='mt-20 text-center underline font-bold'>Apply here </p>
-
-    <div className="container">
-        {/* <iframe
-      title="Nike Product Order Form"
-      className='mb-5'
-      src="https://form.jotform.com/240826909263260"
-      style={{ minWidth: '100%', maxWidth: '100%', height: '539px', border: 'none' }}
-      scrolling="no"
-    /> */}
-
-    <NikeOrderForm/>
-
-    </div>
+      <NikeOrderForm/>
     </>
   )
 }
